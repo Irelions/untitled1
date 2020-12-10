b2fc0e54ea7ac7f1e820cc90cda35699fdf920a3
irelion <irelion@mail.ru>
2020-12-10 22:27:19 +0300

    1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,6 +3,12 @@ package org.example;
 import org.example.config.Config;
 import org.example.model.User;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -12,11 +18,52 @@ import java.util.List;
  */
 public class App 
 {
+    RestTemplate restTemplate = new RestTemplate();
+
+    private final String URL = "http://91.241.64.178:7081/api/users";
+    static String sessionId = getSessionId();
+
+    static HttpHeaders headers = new HttpHeaders();
+
+
+
     public static void main( String[] args ){
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
 
         Communication communication = context.getBean("communication", Communication.class);
-        List<User> allUser = communication.getAllUsers();
-        System.out.println(allUser);
+
+        headers.add();
+
+
+
+        User newUser = new User(3L, "James", "Brown", Byte.parseByte("20"));
+
+
+
+    }
+
+    public String getSessionId () {
+        ResponseEntity<String> responseEntity = restTemplate.exchange(URL, HttpMethod.GET, null, new ParameterizedTypeReference<String>() {});
+        return responseEntity.getHeaders().getFirst("Set-Cookie");
+    }
+
+    public ResponseEntity<String> addUser(User user) {
+        HttpEntity<?> entity = new HttpEntity<>(user, headers);
+
+        return restTemplate
+                .postForEntity(URL, entity, String.class);
+    }
+
+    public User getUserById(int id) {
+        ResponseEntity<User> responseEntity = restTemplate.getForEntity(URL+"/" + id, HttpMethod.GET, null,new ParameterizedTypeReference<User>());
+        User user = restTemplate.getForObject(URL + "/" + id, User.class);
+        return user;
+    }
+
+
+
+    public void deleteUser(int id){
+//        ResponseEntity<>
+
     }
 }
--- src/main/java/org/example/Communication.java
+++ src/main/java/org/example/Communication.java
@@ -3,6 +3,8 @@ package org.example;
 import org.example.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -16,27 +18,12 @@ public class Communication {
     @Autowired
     private RestTemplate restTemplate;
 
-    private final String URL = "http://91.241.64.178:7081/api/users";
 
-    public List<User> getAllUsers() {
-        ResponseEntity<List<User>> responseEntity = restTemplate.exchange(URL, HttpMethod.GET, null, new ParameterizedTypeReference<List<User>>() {
-        });
 
-        List<User> allUser = responseEntity.getBody();
-        System.out.println(responseEntity.getHeaders().getFirst("Set-Cookie"));
-        return allUser;
-    }
 
-    public User getUserById(int id) {
 
-        return null;
-    }
 
-    public void addUser(User user) {
 
-    }
 
-    public void deleteUser(int id){
 
-    }
 }
